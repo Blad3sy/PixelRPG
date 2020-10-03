153d7b72f69a22458cc51866b566682554f0926f
Blad3sy <oscarprosser@gmail.com>
2020-10-03 18:55:49 +0100

    Tests for collision (rocks and flowers) and tilemap functionality
    DId some collision tests to get that working, and made a tilemap to create the map.

--- godot_PixelRPG/Scenes/Player.gd
+++ godot_PixelRPG/Scenes/Player.gd
@@ -1,16 +1,14 @@
 extends KinematicBody2D
 
+export var speed = 75
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var direction: Vector2
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if abs(direction.x) == 1 and abs(direction.y) == 1:
+		direction = direction.normalized()
+	
+	var movement = speed * direction * delta
+	move_and_collide(movement)
